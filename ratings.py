dfa9c54b51bbdf996761a8f883866d73c1791871
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-29 16:35:34 -0700

    Created a function printing out ratings for restaurants and allowing user input for more.

---
+++ ratings.py
@@ -0,0 +1,22 @@
+def restaurant_rating(file_name):
+    """Restaurant rating lister."""
+
+    restaurant_ratings = {}
+
+    with open(file_name) as open_file:
+        for line in open_file:
+            line = line.strip()
+            data = line.split(":")
+            restaurant_ratings[data[0]] = int(data[1])
+
+    new_restaurant = (raw_input("Enter a restaurant name: ")).capitalize()
+    new_rating = raw_input("Enter a restaurante rating: ")
+    restaurant_ratings[new_restaurant] = int(new_rating)
+
+    for restaurant, rating in sorted(restaurant_ratings.items()):
+        print "{} is rated at {}.".format(restaurant, rating)
+
+restaurant_rating("scores.txt")
+
+
+# put your code here
