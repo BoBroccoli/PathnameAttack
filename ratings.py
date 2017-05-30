89ff15dd5e149eaaa69678f8a12353f25188ef63
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-29 18:01:19 -0700

    Added try/except and other commands that will give error messages any time input is not an integer between 1 and 5.

--- ratings.py
+++ ratings.py
@@ -7,11 +7,29 @@ def restaurant_rating(file_name):
         for line in open_file:
             line = line.strip()
             data = line.split(":")
-            restaurant_ratings[data[0]] = int(data[1])
+            restaurant, rating = data
+            restaurant_ratings[restaurant] = rating
 
-    new_restaurant = (raw_input("Enter a restaurant name: ")).capitalize()
-    new_rating = raw_input("Enter a restaurant rating: ")
-    restaurant_ratings[new_restaurant] = int(new_rating)
+    while True:
+        new_restaurant = (raw_input("Enter a restaurant name: ")).capitalize()
+        new_rating = raw_input("Enter a restaurant rating: ")
+
+        try:
+            float(new_rating)
+        except ValueError or False or AttributeError or SyntaxError:  
+            print "Invalid input. Please enter an integer between 1 and 5."
+            continue
+
+        if "." in new_rating:
+            print "Invalid input. Please enter an integer between 1 and 5."
+            continue
+
+        if int(new_rating) < 1 or int(new_rating) > 5:
+            print "Invalid input. Please enter an integer between 1 and 5."    
+            continue
+        else:
+            restaurant_ratings[new_restaurant] = int(new_rating)
+            break
 
     for restaurant, rating in sorted(restaurant_ratings.items()):
         print "{} is rated at {}.".format(restaurant, rating)
